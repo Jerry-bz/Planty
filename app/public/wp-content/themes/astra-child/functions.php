47ffd3f5c4c4e68893f1d364d74ab239514c0372
jerry-bz <jerry.brzt@gmail.com>
2024-08-21 17:42:56 +0200

    réalisation page accueil + page nous rencontrer

--- app/public/wp-content/themes/astra-child/functions.php
+++ app/public/wp-content/themes/astra-child/functions.php
@@ -1,10 +1,13 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles()
-{
+
+function theme_enqueue_styles() {
+    // Charger le style général du thème parent
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	
+    // Charger le style général du thème enfant
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
 
 }
 
-?>
